086c462215d392256c9c227ed3ac3710e41ee7db
lrrd <github@lrrd.anonaddy.com>
2026-02-22 10:55:00 +0800

    feat: add Event type and refactor tasks type

--- types.ts
+++ types.ts
@@ -10,3 +10,34 @@ export interface TimeSlot {
 }
 
 export type SlotError = "INVALID_RANGE" | "OVERLAP" | "TIME_EXCEEDED";
+
+type Weight = 1 | 2 | 3 | 4;
+
+export interface Event {
+  id: string;
+  name: string;
+  notes?: string;
+
+  start: number;
+  end: number;
+
+  isBusy: boolean;
+  buffer: {
+    before: number;
+    after: number;
+  };
+}
+
+export interface Task extends Event {
+  isDone: boolean;
+  isAutoScheduled: boolean;
+}
+
+export interface AutoTask extends Omit<Task, "start" | "end"> {
+  duration: number; // Up to 1440 (23 hours and 30 minutes)
+  weight: Weight; // 1-4 with 4 being the highest
+  slotId: string;
+
+  start?: number;
+  end?: number;
+}

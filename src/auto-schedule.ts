8987a806bdaae4ec886499639967ce612a521a0e
lrrd <github@lrrd.anonaddy.com>
2026-02-23 23:13:01 +0800

    refactor: improve algorithm by implementing a recursion
    - Moved the mock tasks to mock-data.ts.

--- src/auto-schedule.ts
+++ src/auto-schedule.ts
@@ -1,87 +1,56 @@
 import { TOTAL_TIME } from "./constants";
 import type { AutoTask, Result, Event } from "./types";
-import { events } from "./mock-data";
+import { events, queuedTasks } from "./mock-data";
 
 interface MutationData {
   tasks: AutoTask[];
   queue: AutoTask[];
 }
 
-// say this is the queuedTasks
-const tasks: AutoTask[] = Array.from({ length: 10 }, (_, i) => {
-  const rand = Math.floor(Math.random() * 4) + 1;
-  return {
-    // id: crypto.randomUUID(),
-    duration: Math.floor(Math.random() * 240),
-    weight: rand as 1 | 2 | 3 | 4,
-    // isAutoScheduled: true,
-    // name: `Task ${i + 1}`,
-    // description: "",
-    // isBusy: true,
-    // isDone: true,
-    // buffer: { before: 0, after: 0 },
-  };
-});
-
 const scheduleTasks = (
-  queuedTasks: AutoTask[],
-  activeEvents: Event[],
+  queuedTasks: readonly AutoTask[],
+  activeEvents: readonly Event[],
+  toSchedule: AutoTask[] = [],
+  usedTime = 0,
 ): Result<MutationData, ""> => {
-  let queue: AutoTask[] = []; // leftover queue
-  const newTasks = queuedTasks
-    .map((e) => ({ ...e }))
-    .sort((a, b) => a.weight - b.weight); // scheduled tasks
+  const busyEvents =
+    toSchedule.length === 0
+      ? activeEvents.filter((e) => e.isBusy)
+      : activeEvents;
+  const [task, ...tasks] = queuedTasks;
 
-  const busyEvents = activeEvents
-    .filter((e) => e.isBusy)
-    .map((e) => ({ start: e.start, end: e.end, duration: e.end - e.start }));
+  if (!task) return { ok: true, data: { tasks: toSchedule, queue: [] } };
 
-  let usedTime = 0;
-  for (let i = 0; i < newTasks.length; i++) {
-    const task = newTasks[i];
-    if (!task) continue;
+  const totalTime = usedTime + task.duration;
+  const isOverlap = busyEvents.find(
+    (e) => e.end > usedTime && totalTime > e.start,
+  );
 
-    const totalTime = usedTime + task.duration;
-    const isOverlap = busyEvents.find(
-      (e) => e.end > usedTime && totalTime > e.start,
-    );
+  if (isOverlap)
+    return scheduleTasks(tasks, busyEvents, [...toSchedule], isOverlap.end);
 
-    if (isOverlap) {
-      console.log(`overlap: ${i}; usedTime before: ${usedTime}`);
-      usedTime = isOverlap.end;
-      i--;
-      console.log(`overlap: ${i}; usedTime after: ${usedTime}`);
-      continue;
-    }
-
-    const isScheduleFull = totalTime > TOTAL_TIME;
-    if (isScheduleFull) {
-      queue = newTasks.slice(i);
-      newTasks.length = i;
-    }
-
-    task.start = usedTime;
-    task.end = totalTime;
-    usedTime = totalTime;
+  const isScheduleFull = totalTime > TOTAL_TIME;
+  if (isScheduleFull) {
+    return { ok: true, data: { tasks: toSchedule, queue: [task, ...tasks] } };
   }
 
-  return {
-    ok: true,
-    data: { tasks: newTasks, queue: queue },
+  const newTask: AutoTask = {
+    ...task,
+    start: usedTime,
+    end: totalTime,
   };
+
+  return scheduleTasks(tasks, busyEvents, [...toSchedule, newTask], totalTime);
 };
 
-// console.log("tasks", tasks);
-// console.log("assigned", scheduleTasks(tasks, events));
-// console.log("after-tasks", tasks);
-
-const scheduled = scheduleTasks(tasks, events);
-
-if (scheduled.ok) console.log(scheduled.data);
-
 console.time("heavy-task");
-scheduleTasks(tasks, events);
+const scheduled = scheduleTasks(
+  queuedTasks.sort((a, b) => a.weight - b.weight),
+  events,
+);
 console.timeEnd("heavy-task");
+
+// if (scheduled.ok) console.log(scheduled.data);
 
 // just plan out the data structure first
 // since the auto schedule only occurs in 1 (or 2) week

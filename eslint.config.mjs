b7bdea9e7156a90081e0827eafd29988e7fe341a
lrrd <github@lrrd.anonaddy.com>
2026-02-23 19:18:57 +0800

    build:  add eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import eslint from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
+import globals from "globals";
+
+export default defineConfig(
+  eslint.configs.recommended,
+  tseslint.configs.strict,
+  tseslint.configs.stylistic,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+      parserOptions: {
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+);

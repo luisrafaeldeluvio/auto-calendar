ffc99148227621d1a8ec1914d05475f9fa245b3c
lrrd <github@lrrd.anonaddy.com>
2026-02-23 23:15:32 +0800

    build: add eslint-plugin-functional

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,16 @@
 import eslint from "@eslint/js";
 import { defineConfig } from "eslint/config";
 import tseslint from "typescript-eslint";
+import functional from "eslint-plugin-functional";
 import globals from "globals";
 
 export default defineConfig(
   eslint.configs.recommended,
   tseslint.configs.strict,
   tseslint.configs.stylistic,
+  functional.configs.externalTypeScriptRecommended,
+  functional.configs.recommended,
+  functional.configs.stylistic,
   {
     languageOptions: {
       globals: {
@@ -15,6 +19,7 @@ export default defineConfig(
       },
       parserOptions: {
         tsconfigRootDir: import.meta.dirname,
+        projectService: true,
       },
     },
   },

d44106c19ee23070c876bb4d87fda7ef861d000b
lrrd <github@lrrd.anonaddy.com>
2026-02-28 09:25:04 +0800

    build: add react with vite

---
+++ eslint.config.js
@@ -0,0 +1,38 @@
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import functional from "eslint-plugin-functional";
+import { defineConfig, globalIgnores } from "eslint/config";
+
+export default defineConfig([
+  globalIgnores(["dist"]),
+  {
+    files: ["**/*.{ts,tsx}"],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.strict,
+      tseslint.configs.stylistic,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+      functional.configs.externalTypeScriptRecommended,
+      functional.configs.recommended,
+      functional.configs.stylistic,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+      parserOptions: {
+        tsconfigRootDir: import.meta.dirname,
+        projectService: true,
+      },
+    },
+    plugins: {
+      functional,
+    },
+  },
+]);

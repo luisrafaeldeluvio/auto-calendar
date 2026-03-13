27560a03ea2f0f072c5109a0fa96d3a89f27b845
lrrd <github@lrrd.anonaddy.com>
2026-03-13 09:41:56 +0800

    refactor: clean up code

--- src/engine/agenda.ts
+++ src/engine/agenda.ts
@@ -1,32 +1,3 @@
-// slots and events can only be up to 24 hours, events can span days.
-// agenda will be responsible for managing the current flow of time, which is two weeks.
-// it will take the tasks and events that is in the current two weeks.
-
-// agenda will have its own set of data where the tasks and events for the weeks will be placed.
-// but this means that we will have to first work on the tasks due date and start date
-// for the management to make sense.
-
-// Nope, we need to make the agendas first to manage the DATES.
-
-// Lets work on the tasks start date first. add a startDate type (as a unix time) on AutoTask,
-// then lets work on the agenda. the agenda will pull the tasks where their startDate falls within
-// two weeks timeframe. that is what we'll be using.
-
-// for the agenda we will have an object where each key is the day
-// and each value is an object of events and tasks.
-// Ex:
-// UNIX_TIME: {
-//     tasks: [],
-//     events: [],
-// }
-
-// so when getting the tasks and events, the agenda will sort it there.
-
-// FUCCKKKK im confused on how to implement this. write it down and visualize it.
-
-// technically the timefram is just week + 1, because when the current week is over, we'll just move to
-// next week + 1 again.
-
 import {
   areIntervalsOverlapping,
   eachDayOfInterval,
@@ -36,7 +7,6 @@ import {
 import type { AutoTask } from "./types";
 import { tasks } from "./mock-data";
 import { getTime } from "date-fns";
-import { scheduleTasks } from "./auto-schedule";
 
 interface Range {
   start: number;
@@ -93,14 +63,6 @@ const newTimespan = (currentDate: number) => {
 // means its due today, we should be putting tasks on the day where their value is
 // more than or equal to 0.
 
-// ------------------------------------------------------------------------
-
-// maybe this should be a key value array?
-// like [date]: tasks[]?
-// or should that happen in scheduleTask?
-// no because scheduleTasks is only for the dayjsLocalizer
-// so maybe the key value should be here
-// it should be based on their startDate and dueDate;
 const getActiveTasks = (tasks: readonly AutoTask[], range: Readonly<Range>) =>
   tasks.filter((t) =>
     areIntervalsOverlapping(range)({ start: t.startDate, end: t.dueDate }),

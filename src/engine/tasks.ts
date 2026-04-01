9f18505ef4df84955d4ec4e2e95792bcb7be40a8
lrrd <github@lrrd.anonaddy.com>
2026-04-01 17:21:48 +0800

    feat: update createAutoTask to include error handling

--- src/engine/tasks.ts
+++ src/engine/tasks.ts
@@ -1,11 +1,25 @@
 import type { Result, AutoTask } from "./types";
 
+type AutoTaskError =
+  | "INVALID_START_DATE"
+  | "INVALID_DUE_DATE"
+  | "INVALID_DATE_RANGE";
+
 export const createAutoTask = (
   autoTask: Readonly<
     Omit<AutoTask, "id" | "isBusy" | "isDone"> &
       Partial<Pick<AutoTask, "isBusy">>
   >,
-): Result<AutoTask, ""> => {
+): Result<AutoTask, AutoTaskError> => {
+  if (Number.isNaN(autoTask.startDate))
+    return { ok: false, error: "INVALID_START_DATE" };
+
+  if (Number.isNaN(autoTask.dueDate))
+    return { ok: false, error: "INVALID_DUE_DATE" };
+
+  if (autoTask.startDate > autoTask.dueDate)
+    return { ok: false, error: "INVALID_DATE_RANGE" };
+
   const newAutoTask = {
     ...autoTask,
     id: crypto.randomUUID(),

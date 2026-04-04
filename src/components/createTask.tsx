4a9e120d0e114c28c7a99ffb97cfca20cb81a029
lrrd <github@lrrd.anonaddy.com>
2026-04-04 13:25:37 +0800

    feat: create component for creating auto tasks

---
+++ src/components/createTask.tsx
@@ -0,0 +1,184 @@
+import { useEffect, useRef, useState } from "react";
+import { addAutoTask, getAllTimeSlot } from "../db/helpers";
+import { type TimeSlot, type Weight } from "../engine/types";
+import { createAutoTask } from "../engine/tasks";
+import { getTime } from "date-fns";
+
+const durationOptions = [
+  {
+    text: "5 minutes",
+    duration: 5,
+  },
+  {
+    text: "10 minutes",
+    duration: 10,
+  },
+  {
+    text: "15 minutes",
+    duration: 15,
+  },
+  {
+    text: "20 minutes",
+    duration: 20,
+  },
+  {
+    text: "25 minutes",
+    duration: 25,
+  },
+  {
+    text: "30 minutes",
+    duration: 30,
+  },
+  {
+    text: "35 minutes",
+    duration: 35,
+  },
+  {
+    text: "40 minutes",
+    duration: 40,
+  },
+  {
+    text: "45 minutes",
+    duration: 45,
+  },
+  {
+    text: "50 minutes",
+    duration: 50,
+  },
+  {
+    text: "55 minutes",
+    duration: 55,
+  },
+  {
+    text: "60 minutes",
+    duration: 60,
+  },
+];
+
+const weightOptions = [
+  {
+    text: "Low",
+    weight: 0,
+  },
+  {
+    text: "Normal",
+    weight: 1,
+  },
+  {
+    text: "High",
+    weight: 2,
+  },
+  {
+    text: "Do ASAP",
+    weight: 3,
+  },
+];
+
+const createTaskFromForm = (data: FormData) => {
+  const task = createAutoTask({
+    name: String(data.get("name")),
+    notes: String(data.get("notes") ?? ""),
+
+    isBusy: true,
+
+    duration: Number(data.get("durations")),
+    weight: Number(data.get("weight")) as Weight,
+    slotId: String(data.get("timeslots")),
+
+    buffer: {
+      before: 0,
+      after: 0,
+    },
+
+    startDate: getTime(String(data.get("startDate"))),
+    dueDate: getTime(String(data.get("dueDate"))),
+  });
+
+  if (task.ok) {
+    console.log(task.data);
+    addAutoTask(task.data);
+  } else {
+    console.error(task.error);
+  }
+};
+
+export const CreateTaskButton = () => {
+  const dialogRef = useRef<HTMLDialogElement>(null);
+  const [slots, setSlots] = useState<TimeSlot[]>();
+
+  const toggleDialog = () => {
+    if (dialogRef.current) {
+      dialogRef.current.togglePopover();
+    }
+  };
+
+  useEffect(() => {
+    let ignore = false;
+
+    const getslots = async () => {
+      console.log(getTime("null"));
+      return await getAllTimeSlot();
+    };
+
+    getslots().then((s) => setSlots(s));
+
+    return () => {
+      ignore = true;
+    };
+  }, []);
+
+  return (
+    <>
+      <dialog ref={dialogRef} popover="manual">
+        <p>this popped?</p>
+        <form action={createTaskFromForm}>
+          <input type="text" name="name" required />
+
+          <select name="durations" required>
+            {durationOptions.map((e) => {
+              return (
+                <option value={e.duration} key={e.duration}>
+                  {e.text}
+                </option>
+              );
+            })}
+          </select>
+
+          {weightOptions.map((e) => {
+            return (
+              <>
+                <input
+                  type="radio"
+                  name="weight"
+                  id={e.text}
+                  value={e.weight}
+                  key={e.weight}
+                  defaultChecked={e.text === "Normal" ? true : false}
+                  required
+                />
+                <label htmlFor={e.text}>{e.text}</label>
+              </>
+            );
+          })}
+
+          <select name="timeslots" required>
+            {slots?.map((s) => {
+              return (
+                <option value={s.id} key={s.id}>
+                  {s.name}
+                </option>
+              );
+            })}
+          </select>
+
+          <input type="date" name="startDate" required />
+          <input type="date" name="dueDate" required />
+
+          <button type="submit">Create</button>
+        </form>
+        <button onClick={toggleDialog}>Close</button>
+      </dialog>
+      <button onClick={toggleDialog}>Create new task</button>
+    </>
+  );
+};

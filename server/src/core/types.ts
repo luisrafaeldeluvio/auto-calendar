01c1c682e23d8330f08168bdf8a97bdf7def2f64
lrrd <github@lrrd.anonaddy.com>
2026-07-04 15:25:43 +0800

    feat: replaced time-based items with temporal types

--- server/src/core/types.ts
+++ server/src/core/types.ts
@@ -1,42 +1,49 @@
+import { Temporal } from "@js-temporal/polyfill";
+
 export type Result<T, E = string> =
   | { ok: true; data: T }
   | { ok: false; error: E };
 
-export interface TimeSlot {
-  id: string;
-  name: string;
-  start: number; //in minutes since 00:00
-  end: number;
-  isFromOverlap?: boolean;
-}
 export type SlotError = "INVALID_RANGE" | "TIME_EXCEEDED";
 
 export type Weight = 1 | 2 | 3 | 4;
 
-export interface Event {
+export interface TimeSlot {
   id: string;
   name: string;
-  notes?: string;
-
-  start: number; // unix in ms
-  end: number; // unix in ms
-
-  isBusy: boolean;
+  start: Temporal.PlainTime;
+  end: Temporal.PlainTime;
 }
 
-export interface Task extends Event {
+export interface Event {
+  type: "event" | "task";
+  id: string;
+  name: string;
+  notes: string;
+
+  start: Temporal.PlainTime | null;
+  end: Temporal.PlainTime | null;
+  // waitt i think currying can be used here!!!!!
+  // so in our scheduleTasksInSlot, we assign minutes there from 0 to 1440 (full 24h)
+  // but in agenda, this will be turned into their dates, so what if, on the scheduleTasksInSlot,
+  // for the start and end we just return a function, that params a Temporal.PlainDateTime and then
+  // add the minutes + the param to return the final Temporal.PlainDateTime..
+  // wait maybe just set it to Temporal.PlainTime
+
+  isBusy: boolean;
   isDone: boolean;
   isSortable: boolean;
+  isSorted: boolean;
 
-  duration: number; // Up to 1440 (23 hours and 30 minutes)
-  weight: Weight; // 1-4 with 4 being the highest
+  duration: Temporal.Duration | null;
+  weight: Weight;
   slotId: string;
 
   buffer: {
-    before: number;
-    after: number;
+    before: Temporal.Duration | null;
+    after: Temporal.Duration | null;
   };
 
-  startDate: number; // unix in ms
-  dueDate: number; // unix in ms
+  startDate: Temporal.PlainDateTime | null;
+  dueDate: Temporal.PlainDateTime | null;
 }

0de42992894da4e75691438ea1e3df7b409b375a
lrrd <github@lrrd.anonaddy.com>
2026-07-07 00:27:10 -0400

    feat: made start and end of Event to use generics

--- server/src/core/types.ts
+++ server/src/core/types.ts
@@ -14,26 +14,22 @@ export interface TimeSlot {
   end: Temporal.PlainTime;
 }
 
-export interface Event {
+export type EventInterval = Temporal.PlainTime | Temporal.PlainDateTime | null;
+
+export interface Event<T extends EventInterval = null> {
   type: "event" | "task";
   id: string;
   name: string;
   notes: string;
 
-  start: Temporal.PlainTime | null;
-  end: Temporal.PlainTime | null;
-  // waitt i think currying can be used here!!!!!
-  // so in our scheduleTasksInSlot, we assign minutes there from 0 to 1440 (full 24h)
-  // but in agenda, this will be turned into their dates, so what if, on the scheduleTasksInSlot,
-  // for the start and end we just return a function, that params a Temporal.PlainDateTime and then
-  // add the minutes + the param to return the final Temporal.PlainDateTime..
-  // wait maybe just set it to Temporal.PlainTime
+  start:  T;
+  end:  T;
 
   isBusy: boolean;
   isDone: boolean;
   isSortable: boolean;
   isSorted: boolean;
-
+    
   duration: Temporal.Duration | null;
   weight: Weight;
   slotId: string;
@@ -45,4 +41,9 @@ export interface Event {
 
   startDate: Temporal.PlainDateTime | null;
   dueDate: Temporal.PlainDateTime | null;
+}
+
+export interface TasksSchedule<T extends EventInterval = null> {
+  sortedTasks: Event<T>[];
+  queue: Event<null>[];
 }

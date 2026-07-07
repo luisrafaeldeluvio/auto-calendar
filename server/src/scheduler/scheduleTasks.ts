80053e7007956ed910e429acb434528ecfe5dd44
lrrd <github@lrrd.anonaddy.com>
2026-07-07 06:53:04 -0400

    chore: added comments

--- server/src/scheduler/scheduleTasks.ts
+++ server/src/scheduler/scheduleTasks.ts
@@ -4,15 +4,22 @@ import { resolveSlotTaskConflicts } from "./resolveSlotTaskConflicts";
 import { scheduleTasksInSlot, } from "./scheduleTasksInSlot";
 
 //per day palang ito
+// Though while this is per day, it can also be considered as
+// scheduling multiple slots within a day. So instead of 
+// recalculating the whole day, we have the option to only
+// calculate a later part of the day.
 export const scheduleTasks = (
   queuedTasks: Event[],
   busyEvents: Event<Temporal.PlainDateTime>[],
   timeSlots: TimeSlot[],
+  // Move to inner recursion.
   sortedTasks: TasksSchedule<Temporal.PlainTime>[] = [],
+  // Hardcode Date or make it optional?
 ): TasksSchedule<Temporal.PlainTime>[] => {
   const [currentSlot, nextSlot, ...slots] = timeSlots;
   if (!currentSlot) return sortedTasks;
-
+  // I should mvoe this outside the recursion and then maybe turn the Id's
+  // into a set.
   const tasks = queuedTasks.filter((t) => t.slotId === currentSlot.id);
 
   const areSlotsOverlapping =
@@ -31,6 +38,8 @@ export const scheduleTasks = (
   return scheduleTasks(
     queuedTasks,
     [...busyEvents, ...sorted.sortedTasks],
+    // Error here because busyEvent's interval is Temporal.PlainDateTime
+    // while sorted.sortedTasks still uses Temporal.PlainTime
     [...(nextSlot ? [nextSlot] : []), ...slots],
     [...sortedTasks, sorted],
   );

d0a02e2cbee4424b68d2f655dc2410f4219aa368
lrrd <github@lrrd.anonaddy.com>
2026-07-09 02:23:07 -0400

    feat: improve database table

--- server/src/db/db.ts
+++ server/src/db/db.ts
@@ -1,28 +1,36 @@
 import { SQL } from "bun";
+import { Temporal } from "@js-temporal/polyfill";
 
-try {
-  const sql = new SQL("sqlite://events.db");
-  await sql`
+const sql = new SQL("sqlite://events.db");
+await sql`
     CREATE TABLE IF NOT EXISTS events (
-        id VARCHAR(36) PRIMARY KEY,
-        type VARCHAR(5) CHECK (type IN ("event", "task")),
-        name VARCHAR(255),
-        notes VARCHAR(255),
-        start DATETIME,
-        end DATETIME,
-        is_busy BOOLEAN,
-        is_done BOOLEAN,
-        is_sortable BOOLEAN,
-        is_sorted BOOLEAN,
-        duration TEXT,
-        buffer_before TEXT,
-        buffer_after TEXT,
-        start_date DATETIME,
-        due_Date DATETIME
+        id TEXT UNIQUE PRIMARY KEY,
+        type TEXT CHECK (type IN ("event", "task")),
+        name TEXT,
+        notes TEXT,
+        start TEXT, -- ISO-8601
+        end TEXT, -- ISO-8601
+        is_busy INTEGER CHECK (is_busy IN (0, 1)),
+        is_done INTEGER CHECK (is_done IN (0, 1)),
+        is_sortable INTEGER CHECK (is_sortable IN (0, 1)),
+        is_sorted INTEGER CHECK (is_sorted IN (0, 1)),
+        duration TEXT, -- UNIX Timestamp
+        weight INTEGER CHECK (weight IN (1, 2, 3, 4)),
+        slotId TEXT,
+        buffer_before TEXT, -- UNIX Timestamp 
+        buffer_after TEXT, -- UNIX Timestamp
+        start_date TEXT, -- ISO-8601
+        due_Date TEXT -- ISO-8601
     ) STRICT
 `;
 
+// const user = await sql<Event>`
+// INSERT INTO events VALUES (${crypto.randomUUID()}, "task", 
+// "Test Task", "", NULL, NULL, false, false, true, false, 
+// ${Temporal.Duration.from({ hours: 2 }).total("seconds")}, 1, "1", NULL, NULL, 
+// ${Temporal.Now.plainDateTimeISO().toString()},
+// ${Temporal.Now.plainDateTimeISO().toString()})
+// `;
 
-} catch (error) {
-  console.log("ERRORRRRRRR:", error);
-}
+const view = await sql`SELECT * FROM events`
+console.log(view)

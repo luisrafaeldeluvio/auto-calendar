1d8c3bd5da20a99393e369543c432d42905090b3
lrrd <github@lrrd.anonaddy.com>
2026-07-19 10:24:50 +0800

    feat: add EventDbModel and TimeSlotDbModel for serializing Temporal types

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,9 +1,9 @@
 import { Dexie, type Table } from "dexie";
-import type { TimeSlot, Event, EventInterval } from "../types/types";
+import type {  EventDbModel, TimeSlotDbModel } from "../types/types";
 
 export const db = new Dexie("AutoCalendarDB") as Dexie & {
-  timeslots: Table<TimeSlot, string>;
-  events: Table<Event<EventInterval>, string>;
+  timeslots: Table<TimeSlotDbModel, string>;
+  events: Table<EventDbModel, string>;
 };
 
 db.version(1).stores({
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,7 +1,8 @@
 import { Temporal } from "@js-temporal/polyfill";
 
 export type Result<T, E = string> =
-  { ok: true; data: T } | { ok: false; error: E };
+  | { ok: true; data: T }
+  | { ok: false; error: E };
 
 export type SlotError = "INVALID_RANGE" | "TIME_EXCEEDED";
 
@@ -14,6 +15,12 @@ export interface TimeSlot {
   end: Temporal.PlainTime;
 }
 
+export interface TimeSlotDbModel {
+  id: string;
+  name: string;
+  start: string;
+  end: string;
+}
 export type EventInterval = Temporal.PlainTime | Temporal.PlainDateTime | null;
 
 export interface Event<T extends EventInterval = null> {
@@ -34,13 +41,36 @@ export interface Event<T extends EventInterval = null> {
   weight: Weight;
   slotId: string;
 
-  buffer: {
-    before: Temporal.Duration | null;
-    after: Temporal.Duration | null;
-  };
+  buffer_before: Temporal.Duration | null; // duration in the ISO 8601 format
+  buffer_after: Temporal.Duration | null; // duration in the ISO 8601 format
 
   startDate: Temporal.PlainDateTime | null;
   dueDate: Temporal.PlainDateTime | null;
+}
+
+export interface EventDbModel {
+  type: "event" | "task";
+  id: string;
+  name: string;
+  notes: string;
+
+  start: string; // date-time in the RFC 9557 format
+  end: string; // date-time in the RFC 9557 format
+
+  isBusy: boolean;
+  isDone: boolean;
+  isSortable: boolean;
+  isSorted: boolean;
+
+  duration: string | null; // duration in the ISO 8601 format
+  weight: Weight;
+  slotId: string;
+
+  buffer_before: string | null; // duration in the ISO 8601 format
+  buffer_after: string | null; // duration in the ISO 8601 format
+
+  startDate: string | null; //  date-time in the RFC 9557 format
+  dueDate: string | null; //  date-time in the RFC 9557 format
 }
 
 export interface TasksSchedule<T extends EventInterval = null> {

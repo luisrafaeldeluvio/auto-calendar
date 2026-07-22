b9bd8171d0a8e65a71f078ef13466cd4dbaa4d91
lrrd <github@lrrd.anonaddy.com>
2026-07-22 14:50:47 +0800

    feat: add function for calling agenda

--- src/components/createTask.tsx
+++ src/components/createTask.tsx
@@ -1,13 +1,13 @@
 import { useEffect, useRef, useState } from "react";
-
-import {
-  type TimeSlot,
-  type TimeSlotDbModel,
-  type Weight,
-  type Event
-} from "../types/types";
+import { type TimeSlotDbModel, type Weight, type Event } from "../types/types";
 import { addEvent, getAllTimeSlots } from "../db/helpers";
 import { Temporal } from "@js-temporal/polyfill";
+import { agenda } from "../core/agenda";
+import { db } from "../db/db";
+import {
+  fromEventDbModel,
+  fromTimeSlotDbModel,
+} from "../db/serializeDataObject";
 
 const durationOptions = [
   {
@@ -80,7 +80,7 @@ const weightOptions = [
 ];
 
 const createTaskFromForm = async (data: FormData) => {
-  const task:Omit<Event<null>, "id"> = {
+  const task: Omit<Event<null>, "id"> = {
     type: "task",
     name: String(data.get("name")),
     notes: String(data.get("notes")),
@@ -90,7 +90,9 @@ const createTaskFromForm = async (data: FormData) => {
     isDone: false,
     isSortable: true,
     isSorted: false,
-    duration: Temporal.Duration.from({minutes: Number(data.get("durations"))}),
+    duration: Temporal.Duration.from({
+      minutes: Number(data.get("durations")),
+    }),
     weight: Number(data.get("weight")) as Weight,
     slotId: String(data.get("timeslots")),
 
@@ -99,10 +101,61 @@ const createTaskFromForm = async (data: FormData) => {
     startDate: Temporal.PlainDateTime.from(String(data.get("startDate"))),
     dueDate: Temporal.PlainDateTime.from(String(data.get("dueDate"))),
   };
-  const x = await addEvent(task)
+  const x = await addEvent(task);
 
-  if(!x.ok) alert(x.error)
+  if (!x.ok) alert(x.error);
+  sortTasks();
 };
+const sortTasks = async () => {
+  const toSort = await db.events
+    .filter((e) => e.isSortable && !e.isSorted)
+    .toArray()
+    .then((arr) => arr.map((e) => fromEventDbModel(e) as Event<null>));
+
+  const busyEvents = await db.events
+    .where({ isBusy: "true" })
+    .toArray()
+    .then((arr) =>
+      arr.map((e) => fromEventDbModel(e) as Event<Temporal.PlainDateTime>),
+    );
+
+  const slots = await getAllTimeSlots().then((r) =>
+    r.ok ? r.data.map((s) => fromTimeSlotDbModel(s)) : [],
+  );
+
+  const ag = agenda(
+    Temporal.PlainDate.from({ month: 7, day: 19, year: 2026 }),
+    Temporal.PlainDate.from({ month: 7, day: 26, year: 2026 }),
+    toSort ?? [],
+    busyEvents ?? [],
+    slots,
+  );
+
+  const toUpdate = ag.sortedTasks.map((e) => {
+    return {
+      key: e.id,
+      changes: {
+        start: e.start.toString(),
+        end: e.start.toString(),
+        isBusy: e.isBusy,
+        isSorted: e.isSortable,
+      },
+    };
+  });
+
+  try {
+    db.events.bulkUpdate(toUpdate);
+  } catch (e) {
+    console.log("on update", e);
+  }
+};
+// - [ ] I think theres something wrong in agenda? I should run the tests.
+// - Yep something is wrong, I got the same DateTime for both start and end.
+// - [ ] need to use the LiveQuery thing since when creating a new slot, its not
+// getting updated on react, resulting in needint to relaod.
+// - [ ]  add startBy and dueBy default of today on the form.
+// - [x] TODO: make it now sort them (via agenda) when adding new tasks.
+// - Maybe a new sort button?
 export const CreateTaskButton = () => {
   const dialogRef = useRef<HTMLDialogElement>(null);
   const [slots, setSlots] = useState<TimeSlotDbModel[]>([]);

3ab91408a0948adb7eb7625076e8942fbe8242e0
lrrd <github@lrrd.anonaddy.com>
2026-07-31 19:23:55 +0800

    refactor: fromEventDbModel to be more safe

--- src/db/modelMappers.ts
+++ src/db/modelMappers.ts
@@ -1,8 +1,12 @@
 import { Temporal } from "@js-temporal/polyfill";
-import type { Event } from "../types/models/calendarItem";
+import type {
+  CalendarEvent,
+  CalendarItem,
+  CalendarTask,
+} from "../types/models/calendarItem";
 import type { TimeSlot } from "../types/models/timeslot";
 import type { TimeSlotDbModel, EventDbModel } from "./types";
-
+import type { Result } from "../types/common";
 
 export const toTimeSlotDbModel = (slot: TimeSlot): TimeSlotDbModel => ({
   ...slot,
@@ -16,9 +20,7 @@ export const fromTimeSlotDbModel = (slot: TimeSlotDbModel): TimeSlot => ({
   end: Temporal.PlainTime.from(slot.end),
 });
 
-export const toEventDbModel = (
-  event: Event<Temporal.PlainDateTime | null>,
-): EventDbModel => ({
+export const toEventDbModel = (event: CalendarItem): EventDbModel => ({
   ...event,
   start: event.start?.toString() ?? "",
   end: event.end?.toString() ?? "",
@@ -29,17 +31,56 @@ export const toEventDbModel = (
   dueDate: event.dueDate?.toString() ?? "",
 });
 
+type FromEventDbModelError = "MISSING_INTERVAL" | "INVALID_INTERVAL_PAIRS";
+
 export const fromEventDbModel = (
   event: EventDbModel,
-): Event<Temporal.PlainDateTime | null> => ({
-  ...event,
-  start: event.start ? Temporal.PlainDateTime.from(event.start) : null,
-  end: event.end ? Temporal.PlainDateTime.from(event.end) : null,
-  duration: Temporal.Duration.from(event.duration),
-  bufferBefore: Temporal.Duration.from(event.bufferBefore),
-  bufferAfter: Temporal.Duration.from(event.bufferAfter),
-  startDate: event.startDate
-    ? Temporal.PlainDateTime.from(event.startDate)
-    : null,
-  dueDate: event.dueDate ? Temporal.PlainDateTime.from(event.dueDate) : null,
-});
+): Result<CalendarItem, FromEventDbModelError> => {
+  const base = {
+    id: event.id,
+    name: event.name,
+    notes: event.notes,
+    isBusy: event.isBusy,
+    isDone: event.isDone,
+    isSortable: event.isSortable,
+    isSorted: event.isSorted,
+    duration: Temporal.Duration.from(event.duration),
+    weight: event.weight,
+    slotId: event.slotId,
+    bufferBefore: Temporal.Duration.from(event.bufferBefore),
+    bufferAfter: Temporal.Duration.from(event.bufferAfter),
+    startDate: Temporal.PlainDateTime.from(event.startDate),
+    dueDate: Temporal.PlainDateTime.from(event.dueDate),
+  };
+
+  const start = event.start ? Temporal.PlainDateTime.from(event.start) : null;
+  const end = event.end ? Temporal.PlainDateTime.from(event.end) : null;
+
+  if (event.type === "event") {
+    if (!start || !end) return { ok: false, error: "MISSING_INTERVAL" };
+
+    return {
+      ok: true,
+      data: {
+        ...base,
+        type: "event",
+        start,
+        end,
+      } as CalendarEvent,
+    };
+  }
+
+  if ((start && !end) || (!start && end))
+    return { ok: false, error: "INVALID_INTERVAL_PAIRS" };
+
+  return {
+    ok: true,
+    data: {
+      ...base,
+      type: "task",
+      start,
+      end,
+    } as CalendarTask,
+  };
+};
+

e5276e73f39b36ea21f34b9894737eb2afddae02
lrrd <github@lrrd.anonaddy.com>
2026-07-31 19:21:36 +0800

    feat: split Event type into CalendarEvent, CalendarTask, and CalendarItem

--- src/types/models/calendarItem.ts
+++ src/types/models/calendarItem.ts
@@ -1,16 +1,10 @@
 import { Temporal } from "@js-temporal/polyfill";
 import type { Weight } from "../common";
 
-export type EventInterval = Temporal.PlainDateTime | null;
-
-export interface Event<T extends EventInterval> {
-  type: "event" | "task";
+interface BaseItem {
   id: string;
   name: string;
   notes: string;
-
-  start: T;
-  end: T;
 
   isBusy: boolean;
   isDone: boolean;
@@ -24,6 +18,22 @@ export interface Event<T extends EventInterval> {
   bufferBefore: Temporal.Duration;
   bufferAfter: Temporal.Duration;
 
-  startDate: Temporal.PlainDateTime | null;
-  dueDate: Temporal.PlainDateTime | null;
+  startDate: Temporal.PlainDateTime;
+  dueDate: Temporal.PlainDateTime;
 }
+
+export interface CalendarEvent extends BaseItem {
+  type: "event";
+  start: Temporal.PlainDateTime;
+  end: Temporal.PlainDateTime;
+}
+
+export interface CalendarTask<
+  T extends null | Temporal.PlainDateTime = Temporal.PlainDateTime,
+> extends BaseItem {
+  type: "task";
+  start: T;
+  end: T;
+}
+
+export type CalendarItem = CalendarEvent | CalendarTask;

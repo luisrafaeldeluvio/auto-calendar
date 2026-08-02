cb573e9d2c68cb5ecba11db2bf49cbb2359c5e93
lrrd <github@lrrd.anonaddy.com>
2026-08-02 22:02:38 +0800

    refactor: add logs to scheduleTasksInSlot for better debugging

--- src/core/scheduleTasksInSlot.ts
+++ src/core/scheduleTasksInSlot.ts
@@ -1,6 +1,10 @@
 import { Temporal } from "@js-temporal/polyfill";
 import type { TasksSchedule } from "../types/common";
-import type { CalendarItem, CalendarTask, CalendarTaskUnscheduled } from "../types/models/calendarItem";
+import type {
+  CalendarItem,
+  CalendarTask,
+  CalendarTaskUnscheduled,
+} from "../types/models/calendarItem";
 
 export const scheduleTasksInSlot = (
   queuedTasks: CalendarTaskUnscheduled[],
@@ -11,7 +15,7 @@ export const scheduleTasksInSlot = (
 ) => {
   const busyEvents = activeEvents.filter((e) => e.isBusy);
   const sortTasks = queuedTasks.toSorted((a, b) => b.weight - a.weight);
-  
+
   // - [ ] should add creationDate property on CalenderBase. So when sorting
   // those with older creationDate will have higher priority
   const schedule = (
@@ -19,13 +23,25 @@ export const scheduleTasksInSlot = (
     currentTime: Temporal.PlainTime,
     sortedTasks: CalendarTask[],
   ): TasksSchedule => {
+    console.log({
+      tasksToProcess: tasksToProcess.map((t) => t.id),
+      currentTime: currentTime.toString(),
+    });
     const [task, ...remainingTasks] = tasksToProcess;
-    if (!task) return { sortedTasks: sortedTasks, queue: [] };
+    if (!task) {
+      console.log("No more tasks to sort, Stopped scheduling tasks");
+      return { sortedTasks: sortedTasks, queue: [] };
+    }
 
     const taskStartTime: Temporal.PlainTime = currentTime;
     const taskEndTime: Temporal.PlainTime = taskStartTime.add(
       task.duration ?? { minutes: 0 },
     );
+    console.log("Processing: ", {
+      id: task.id,
+      proposedStart: taskStartTime.toString(),
+      proposedEnd: taskEndTime.toString(),
+    });
 
     const overlappingEvent: CalendarItem | undefined = busyEvents.find(
       (e) =>
@@ -34,6 +50,13 @@ export const scheduleTasksInSlot = (
         Temporal.PlainTime.compare(e.end, taskStartTime) === 1 &&
         Temporal.PlainTime.compare(taskEndTime, e.start) === 1,
     );
+
+    if (overlappingEvent)
+      console.log(
+        "Overlaps with: ",
+        overlappingEvent.id,
+        " Skipped proposed time",
+      );
 
     if (overlappingEvent && overlappingEvent.end)
       return schedule(
@@ -46,15 +69,32 @@ export const scheduleTasksInSlot = (
       Temporal.PlainTime.compare(taskEndTime, slotEndTime) === 1;
 
     if (isSlotFull) {
+      console.log("Slot is full, Stopped scheduling tasks");
+      console.log({
+        sortedTasks: sortedTasks,
+        queue: [
+          ...remainingTasks,
+          {
+            ...task,
+            start: null,
+            end: null,
+            isSorted: false,
+            isBusy: false,
+          },
+        ],
+      });
       return {
         sortedTasks: sortedTasks,
-        queue: [...remainingTasks, {
-          ...task,
-          start: null,
-          end: null,
-          isSorted: false, 
-          isBusy: false
-        }],
+        queue: [
+          ...remainingTasks,
+          {
+            ...task,
+            start: null,
+            end: null,
+            isSorted: false,
+            isBusy: false,
+          },
+        ],
       };
     }
 
@@ -65,9 +105,10 @@ export const scheduleTasksInSlot = (
       isBusy: true,
       isSorted: true,
     };
-
+    console.log("Added task to schedule object. Scheduling next task");
     return schedule(remainingTasks, taskEndTime, [...sortedTasks, newTask]);
   };
 
+  console.log("Start scheduling tasks");
   return schedule(sortTasks, slotStartTime, []);
 };
